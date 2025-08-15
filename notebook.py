307794ec9d7b75eec3a8de1776f938781847e8fb
jorvarea <jorvarea@free_server.com>
2025-08-15 18:40:51 +0000

    make function to generate next move

--- notebook.py
+++ notebook.py
@@ -2,6 +2,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
 from datasets import load_dataset
 from peft import LoraConfig, get_peft_model, TaskType
+import torch
 
 # %% Load model and tokenizer
 model_name = "unsloth/gemma-3-270m-it"
@@ -21,7 +22,7 @@ lora_config = LoraConfig(
 model = get_peft_model(model, lora_config)
 
 # %% Load dataset and tokenize it
-dataset = load_dataset("Thytu/ChessInstruct", split="train[:10%]")  # 10% para pruebas iniciales
+dataset = load_dataset("Thytu/ChessInstruct", split="train[:1%]")
 
 def combine_input_output(example):
     # Format: <task/instruction> + <input> + <expected_output>
@@ -49,7 +50,7 @@ training_args = TrainingArguments(
     output_dir="./gemma-3-270m-lora",
     per_device_train_batch_size=1,
     gradient_accumulation_steps=4,
-    num_train_epochs=3,
+    num_train_epochs=1,
     logging_steps=10,
     save_steps=50,
     save_total_limit=2,
@@ -69,4 +70,28 @@ trainer.train()
 # %% Save model and tokenizer
 model.save_pretrained("./gemma-3-270m-lora")
 tokenizer.save_pretrained("./gemma-3-270m-lora")
+
+# %% Test model
+def generate_move(moves: list[str], max_new_tokens=16, temperature=0.1, prompt: str = "Given some set of chess moves, write the best possible move"):
+    # Tokenize input
+    input_text = prompt + " " + " ".join(moves)
+    inputs = tokenizer(input_text, return_tensors="pt")
+
+    # Generate output tokens
+    with torch.no_grad():
+        output = model.generate(
+            inputs,
+            max_new_tokens=max_new_tokens,
+            temperature=temperature,
+            pad_token_id=tokenizer.eos_token_id
+        )
+
+    # Decode generated tokens
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
+    return generated_text
+
+# %% Test model
+moves = ["e4e5", "Nf3"]
+
+generate_move(moves)
 

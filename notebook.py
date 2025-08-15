7617ca73a00ebb7f0f4a1557ecb0008fb4beb7f1
jorvarea <jorvarea@free_server.com>
2025-08-15 19:39:30 +0000

    working inference

--- notebook.py
+++ notebook.py
@@ -1,7 +1,7 @@
 # %% Imports
 from transformers import AutoTokenizer, AutoModelForCausalLM, Trainer, TrainingArguments
 from datasets import load_dataset
-from peft import LoraConfig, get_peft_model, TaskType
+from peft import LoraConfig, get_peft_model, TaskType, PeftModel
 import torch
 
 # %% Load model and tokenizer
@@ -69,18 +69,18 @@ trainer.train()
 
 # %% Save model and tokenizer
 model.save_pretrained("./gemma-3-270m-lora")
-tokenizer.save_pretrained("./gemma-3-270m-lora")
 
 # %% Test model
-def generate_move(moves: list[str], max_new_tokens=16, temperature=0.1, prompt: str = "Given some set of chess moves, write the best possible move"):
+def generate_move(moves: list[str], max_new_tokens=8, temperature=0.1, prompt: str = "Given some set of chess moves, write the best possible move") -> str:
     # Tokenize input
     input_text = prompt + " " + " ".join(moves)
     inputs = tokenizer(input_text, return_tensors="pt")
+    inputs = {k: v.to(model.device) for k, v in inputs.items()}
 
     # Generate output tokens
     with torch.no_grad():
         output = model.generate(
-            inputs,
+            **inputs,
             max_new_tokens=max_new_tokens,
             temperature=temperature,
             pad_token_id=tokenizer.eos_token_id
@@ -88,10 +88,18 @@ def generate_move(moves: list[str], max_new_tokens=16, temperature=0.1, prompt: 
 
     # Decode generated tokens
     generated_text = tokenizer.decode(output[0], skip_special_tokens=True)
-    return generated_text
+    return generated_text[len(input_text):].strip()
+
+# %% Load finetuned model
+checkpoint_path = "./gemma-3-270m-lora/checkpoint-248"
+model_base = AutoModelForCausalLM.from_pretrained("unsloth/gemma-3-270m-it")
+model = PeftModel.from_pretrained(model_base, checkpoint_path)
+tokenizer = AutoTokenizer.from_pretrained("unsloth/gemma-3-270m-it")
+model.to("cpu")
 
 # %% Test model
+#moves = ["e2e4", "c7c5", "g1f3", "b8c6", "d2d4", "c5d4", "f3d4", "d7d6", "c1e3", "g7g6", "f2f3", "e7e5", "d4b3", "g8f6", "b1c3", "c8e6", "d1e2", "a7a5", "a2a4", "f8e7", "e1c1", "e8g8", "b3c5", "d6d5", "e4d5", "f6d5", "c3d5", "e6d5", "c5b7", "d8c8", "d1d5", "c8b7", "e2b5", "b7c8", "b5b3", "c6d4", "e3d4", "e5d4", "c1b1", "a8b8", "d5b5", "c8c7", "b3c4", "c7d6", "f1d3", "f8c8", "c4b3", "b8b5", "a4b5", "d6b4", "b3b4", "e7b4", "b1a2", "g8f8", "h1d1", "c8c7", "f3f4", "f8e7", "a2b3", "e7d6", "f4f5", "g6g5", "f5f6", "h7h6", "d1f1", "b4d2", "b3a4", "d2b4", "f1f2", "c7c5", "f2e2", "c5e5", "e2e5", "d6e5", "b5b6", "e5f6", "g2g4", "f6e6", "a4b5", "e6d7", "d3f5", "d7d6"]
 moves = ["e4e5", "Nf3"]
+print(generate_move(moves, max_new_tokens=4))
 
-generate_move(moves)
-
+# %%
